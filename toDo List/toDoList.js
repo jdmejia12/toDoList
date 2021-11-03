b83f249bb681409effa9cda88f7dd5c9c0f964ae
JD <jdmejia712@gmail.com>
2021-11-03 12:22:13 -0400

    completed

--- toDo List/toDoList.js
+++ toDo List/toDoList.js
@@ -1,3 +1,27 @@
+const formJ = document.querySelector('form')
 document.addEventListener('DOMContentLoaded', () =>{
-    formJ.addEventListener('submit', (e)=>{e.preventDefault(); console.log(e.target)})})
+    formJ.addEventListener('submit', (e)=>{
+        e.preventDefault(); 
+        handleToDo(e.target.textBoxtoDo.value)//takes evnt.target'textbox'.targetID.value of target
+        formJ.reset()
+    })
+}) 
+
+function handleToDo(todo){
+    let p = document.createElement('p');
+    let del = document.createElement('button');
+    del.textContent = 'x';
+    p.textContent = `${todo} `;
+    p.appendChild(del);
+    console.log(p)
+    document.querySelector('#list_container').appendChild(p);
     
+    del.addEventListener('click', ()=>{
+        p.remove()
+    })
+}
+
+//alternate to anonymous del function, create callback
+// function delFunk(e){
+//     e.target.parentNode.remove()
+// }
